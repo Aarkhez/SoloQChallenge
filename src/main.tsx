2a2d8d016150f66a79a700c974518150506327c8
Aarkhez <romaniolo99@gmail.com>
2024-10-15 19:57:53 +0200

    test

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import RiotPage from "./pages/RiotPage.tsx";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import DataPage from "./pages/DataPage.tsx";
@@ -13,7 +12,6 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { path: "/", element: <DataPage /> },
-      { path: "/riot", element: <RiotPage /> },
     ],
   },
 ]);
